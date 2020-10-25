27b74c4c535b5f83d80837984999ee811a833bdb
Sayaka Ono <sayaka.ono.81@gmail.com>
2020-10-25 14:42:34 -0700

    install nodemon, add routes folder

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const port = 8080;
 const app = express();
 
+const apiRoutes = require('./routes/api-routes');
+
 app.use(express.json());
 
+app.use('/api', apiRoutes);
 app.get('/', (req, res) => {
   res.status(200);
 });
---
+++ routes/api-routes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+  res.json({
+    status: 'API is working',
+    message: 'Welcome!'
+  });
+});
+
+module.exports = router;
